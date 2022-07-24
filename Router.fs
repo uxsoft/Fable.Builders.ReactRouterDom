2abf4cc1508eb10ec0607c8da5f271ab067c2f10
Jan Dryk <me@uxsoft.cz>
2022-07-24 15:45:49 +0200

    add useNavigate

--- Router.fs
+++ Router.fs
@@ -1,5 +1,6 @@
 module Fable.Builders.ReactRouterDom
 
+open Browser.Types
 open Fable.Builders.Common
 open Fable.Core
 open Fable.Core.JsInterop
@@ -45,3 +46,12 @@ let Outlet = OutletBuilder()
 
 [<ImportMember("react-router-dom")>]
 let useParams<'T>() : 'T = jsNative
+
+[<ImportMember("react-router-dom")>]
+let useNavigate : (unit -> (string -> {| replace: bool; state: obj |} -> unit)) = jsNative
+
+[<ImportMember("react-router-dom")>]
+let useLocation : (unit -> Location) = jsNative
+
+[<ImportMember("react-router-dom")>]
+let useHref : (string -> string) = jsNative
